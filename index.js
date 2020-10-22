fd8a1c84d4bf3765beca84af32be7e51dccaa051
ashwithaDevireddy <47997893+ashwithaDevireddy@users.noreply.github.com>
2020-10-22 15:50:48 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,9 @@ var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
 var randomDiceImage = "dice" + randomNumber1 + ".png";
 
-var randomImageSource = "images/" + randomDiceImage;
-
 var image1 = document.querySelectorAll("img")[0];
 
-image1.setAttribute("src", randomImageSource);
+image1.setAttribute("src", randomDiceImage);
 
 
 
@@ -16,9 +14,7 @@ var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
 var randomDiceImage2 = "dice" + randomNumber2 + ".png";
 
-var randomImageSource2 = "images/" + randomDiceImage2;
-
-document.querySelectorAll("img")[1].setAttribute("src",randomImageSource2);
+document.querySelectorAll("img")[1].setAttribute("src",randomDiceImage2);
 
 
 
